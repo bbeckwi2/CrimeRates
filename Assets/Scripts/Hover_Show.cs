f9bcc9a0d892b92c085af3b5dba847dc42f274b7
aowolfie <bluestar1996zz@gmail.com>
2019-04-01 19:34:07 -0400

    Fixed labels jumping around, added fade out.
    Labels no jump around since the controls are now being treated as
    triggers.
    
    Added a simple fade out to labels.
    
    Fixed label tracking. Now labels always face the player.
    
    There is now a label for each controller, go nuts!

--- Assets/Scripts/Hover_Show.cs
+++ Assets/Scripts/Hover_Show.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class Hover_Show : MonoBehaviour
 {
-    GameObject fontMesh;
+    public GameObject fontMesh;
     Collider collide;
     GameObject bar;
+    public GameObject head;
+    int chance = 0;
+    int maxChance = 50;
+    Collider collideWith;
+    Color color;
+    Material mat;
 
     // Start is called before the first frame update
     void Start() {
-        fontMesh = GameObject.Find("Label_Display");
         collide = fontMesh.GetComponent<BoxCollider>();
+        mat = fontMesh.GetComponent<Material>();
+        color = Color.white;
     }
 
     // Update is called once per frame
@@ -22,24 +29,34 @@ public class Hover_Show : MonoBehaviour
             Vector3 ctr = this.transform.rotation.eulerAngles;
             msh.y = ctr.y;
             fontMesh.transform.rotation = Quaternion.Euler(msh);
+            color.a = 1.0f;
+            fontMesh.GetComponent<MeshRenderer>().material.color = color;
+        } else if (chance > 0) {
+            chance--;
+            color.a = ((float)chance) / ((float)maxChance);
+            fontMesh.GetComponent<MeshRenderer>().material.color = color;
         } else {
             fontMesh.transform.position = new Vector3(0f, -1000f, 0f);
         }
+        fontMesh.transform.LookAt(head.transform);
     }
 
-
-    void OnCollisionEnter(Collision col) {
+    // Draw the label when we put our hand into the bar
+    private void OnTriggerEnter(Collider col) {
         if (col.gameObject.name == "Bar(Clone)" && bar == null) {
             TextMesh m = fontMesh.GetComponent<TextMesh>();
             Bar b = col.gameObject.GetComponent<Bar>();
             print(b);
             m.text = b.getLabel();
             bar = col.gameObject;
+            chance = maxChance;
+            collideWith = col;
         }
     }
 
-    void OnCollisionExit(Collision col) {
-        if (bar != null) {
+    // Remove the label when remove our hand
+    void OnTriggerExit(Collider col) {
+        if (col == collideWith) {
             bar = null;
         }
     }
